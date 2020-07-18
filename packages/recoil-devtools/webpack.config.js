07bd4fe1a9fdc801e60140beb09ff881018027d6
ulises-jeremias <ulisescf.24@gmail.com>
2020-07-18 17:29:50 -0300

    Added core package

---
+++ packages/recoil-devtools/webpack.config.js
@@ -0,0 +1,43 @@
+const config = {
+  entry: './index.d.tsx',
+  output: {
+    filename: 'lib/[name].js',
+  },
+  module: {
+    rules: [{
+      enforce: 'pre',
+      test: /\.(ts|tsx)$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'tslint-loader',
+      options: {
+        failOnWarning: false,
+        failOnError: true,
+      },
+    },
+    {
+      test: /\.tsx?$/,
+      use: [
+        {
+          loader: 'awesome-typescript-loader',
+          options: {
+            silent: true,
+            useBabel: true,
+            babelOptions: {
+              babelrc: false,
+              compact: process.env.NODE_ENV === 'production',
+              highlightCode: true,
+            },
+            babelCore: '@babel/core',
+            useCache: true,
+          }
+        },
+      ],
+      exclude: /(node_modules|bower_components)/
+    }],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+};
+
+module.exports = config;
