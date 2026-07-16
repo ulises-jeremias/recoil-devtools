f0bd27aa86159ce41c033b167b215529126469fb
Yurii Bakurov <45154988+Yurii201811@users.noreply.github.com>
2026-07-16 17:15:58 +0200

    fix(dock): correct LogMonitor repository link

--- packages/recoil-devtools-dock/test/blah.test.tsx
+++ packages/recoil-devtools-dock/test/blah.test.tsx
@@ -14,6 +14,18 @@ describe('Render', () => {
 });
 
 describe('DockMonitor validation', () => {
+  it('links to the Recoil LogMonitor for missing children', () => {
+    const error = vi.spyOn(console, 'error').mockImplementation(() => {});
+
+    render(<DockMonitor>{[]}</DockMonitor>);
+
+    expect(error).toHaveBeenCalledWith(
+      '<DockMonitor> requires at least one monitor inside. ' +
+        'Why dont you try <LogMonitor>? You can get it at ' +
+        'https://github.com/ulises-jeremias/recoil-devtools/tree/main/packages/recoil-devtools-log-monitor.'
+    );
+  });
+
   it('reports the missing monitor switch key for multiple children', () => {
     const error = vi.spyOn(console, 'error').mockImplementation(() => {});
 
@@ -37,7 +49,7 @@ describe('DockMonitor validation', () => {
     expect(error).not.toHaveBeenCalledWith(
       '<DockMonitor> requires at least one monitor inside. ' +
         'Why don’t you try <LogMonitor>? You can get it at ' +
-        'https://github.com/reduxjs/redux-devtools/tree/master/packages/redux-devtools-log-monitor.'
+        'https://github.com/ulises-jeremias/recoil-devtools/tree/main/packages/recoil-devtools-log-monitor.'
     );
   });
 });
